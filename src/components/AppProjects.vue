bba00f32982ec3f2c7c3b398c619ed88df5b801b
ytrav <withertrav394@gmail.com>
2022-08-19 10:50:21 +0200

    Popup almost done

--- src/components/AppProjects.vue
+++ src/components/AppProjects.vue
@@ -22,14 +22,22 @@
     </Transition>
     <Transition name="pop">
       <div v-if="popup" class="popup">
-      <div @click="popup=false" class="close">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" /></svg>
-      </div>
         <img :src="project.img" :alt="project.alt" />
         <div class="popupinfo">
-          <h3>{{ project.name }}</h3>
-          <span>{{ project.year }}</span>
+          <div @click="popup = false" class="close">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path
+                d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
+              />
+            </svg>
+          </div>
+          <h1>{{ project.name }}</h1>
+          <h3>{{ project.year }}</h3>
           <p>{{ project.desc }}</p>
+          <div id="pop-cta">
+            <a class="primary-button" href="#">Try it</a>
+            <a class="secondary-button" href="#">See code on GitHub</a>
+          </div>
         </div>
       </div>
     </Transition>
@@ -206,20 +214,105 @@ p {
 
 .popup {
   position: fixed;
-  top: 5vh;
-  left: 5vw;
-  right: 5vw;
-  bottom: 5vh;
+  top: 10vh;
+  left: 10vw;
+  right: 10vw;
+  bottom: 10vh;
   background-color: $primary;
   color: $bg1;
   z-index: 13;
-  border-radius: 10px;
+  border-radius: 15px;
+  overflow: auto;
   padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  gap: 50px;
+  align-content: stretch;
   img {
     // zoom: 30%;
-    object-fit: fit;
-    width: 100%;
+    object-fit: cover;
+    // width: 100%;
+    // max-width: 500px;
+    max-width: 70%;
+    max-height: 100%;
     border-radius: 10px 10px 0 0;
+    flex-grow: 0;
+    flex-shrink: 2;
+    border-radius: 15px;
+  }
+  .popupinfo {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    flex-grow: 1;
+    #pop-cta {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      justify-self: flex-end;
+      a {
+        text-decoration: none;
+        margin-top: 15px;
+        text-align: center;
+        align-self: center;
+        border-radius: 5000px;
+        width: 200px;
+        padding: 5px 20px 5px 20px;
+        transition: background-color 0.1s ease-out;
+      }
+    }
+    h1 {
+      line-height: 35px;
+      margin-right: 30px;
+    }
+    p {
+      max-width: 300px;
+      text-align: justify;
+    }
+    .primary-button {
+      color: $primary;
+      background-color: $bg1;
+      // border: 2px solid $bg1;
+      &:hover {
+        background-color: #d4e4e6;
+      }
+      &:active {
+        background-color: #abb8b9;
+      }
+    }
+    .secondary-button {
+      color: $bg1;
+      border: 2px solid $bg1;
+      &:hover {
+        background-color: #004483;
+      }
+      &:active {
+        background-color: #004c94;
+      }
+    }
+  }
+  /* ===== Scrollbar CSS ===== */
+  /* Firefox */
+  scrollbar-width: thin;
+  scrollbar-color: #efffff $primary;
+
+  /* Chrome, Edge, and Safari */
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: $primary;
+    border-radius: 12px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: $bg1;
+    border-radius: 10px;
+    border: 2px none #ffffff;
+    &:hover {
+      background-color: #c6d9db;
+    }
   }
 }
 
@@ -228,6 +321,77 @@ p {
   cursor: pointer;
   width: 30px;
   height: 30px;
+  position: absolute;
+  border-radius: 50%;
+  backdrop-filter: blur(5px);
+  top: 20px;
+  right: 20px;
+  &:hover {
+    svg {
+      fill: #c6d9db;
+    }
+  }
+  &:active {
+    svg {
+      fill: #abb8b9;
+    }
+  }
 }
 
+@media only screen and (max-width: 1148px) and (orientation: portrait) {
+  .popup {
+    flex-direction: column;
+    flex-wrap: nowrap;
+    align-content: center;
+    justify-content: center;
+    padding: 60px 20px 20px 20px;
+    img {
+      max-width: 100%;
+    }
+    .popupinfo {
+      align-items: stretch;
+      p {
+        max-width: 100%;
+      }
+      .primary-button,
+      .secondary-button {
+        align-self: flex-start;
+      }
+    }
+  }
+}
+
+@media only screen and (max-width: 1111px) and (orientation: landscape) {
+  .popup {
+    img {
+      max-width: 60%;
+    }
+  }
+}
+
+@media only screen and (max-width: 540px) {
+  .popup {
+    .popupinfo {
+      .primary-button,
+      .secondary-button {
+        width: 100%;
+      }
+    }
+  }
+}
+
+@media only screen and (max-width: 400px), (max-height: 411px) {
+  .popup {
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    border-radius: 0;
+    .close {
+      position: fixed;
+      top: 15px;
+      right: 15px;
+    }
+  }
+}
 </style>
